76228b8328ee5d6f9c64829f313e270fae8acf3c
Xiangmingchen <31719253+Xiangmingchen@users.noreply.github.com>
2017-11-09 13:31:47 -0600

    added new fields to database

--- RMAapp/app/models.py
+++ RMAapp/app/models.py
@@ -1,9 +1,9 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, Float
+from sqlalchemy import Column, ForeignKey, Integer, String, Float, Text
 from sqlalchemy.orm import relationship
-from app.database import Base
+from app import db
 
 
-class Apartment(Base):
+class Apartment(db.Model):
     __tablename__ = 'apartment'
     # Here we define columns for the table Apartment
     # Notice that each column is also a normal Python instance attribute.
@@ -11,8 +11,13 @@ class Apartment(Base):
     zpid = Column(Integer, nullable=False)
     address = relationship('Address', backref='apartment')
     rentPerMonth = Column(Float)
+    image = relationship('Image', backref='apartment')
+    review = relationship('Review', backref='apartment')
 
-class Address(Base):
+    def __repr__(self):
+        return '<Apartment %r>' % (self.id)
+
+class Address(db.Model):
     __tablename__ = 'address'
     # Here we define columns for the table address.
     id = Column(Integer, primary_key=True)
@@ -21,3 +26,27 @@ class Address(Base):
     city = Column(String(50))
     state = Column(String(20))
     apartment_id = Column(Integer, ForeignKey('apartment.id'))
+
+    def __repr__(self):
+        return '<Address %r>' % (self.street + ' ' + self.zipcode + ' ' + self.city + ' ' + self.state)
+
+class Image(db.Model):
+    __tablename__ = 'image'
+
+    id = Column(Integer, primary_key=True)
+    url = Column(Text, nullable=False)
+    apartment_id = Column(Integer, ForeignKey('apartment.id'))
+
+    def __repr__(self):
+        return '<URL %r>' % (self.url)
+
+class Review(db.Model):
+    __tablename__ = 'review'
+
+    id = Column(Integer, primary_key=True)
+    user_name = Column(String(50), nullable=False)
+    content = Column(Text, nullable=False)
+    apartment_id = Column(Integer, ForeignKey('apartment.id'))
+
+    def __repr__(self):
+        return '<Review from user: %r>' % (self.user_name)
