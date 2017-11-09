4c4d13e3a0ed9ac13c25c7d7ec7b3f1bdc443e8d
Xiangmingchen <31719253+Xiangmingchen@users.noreply.github.com>
2017-11-09 17:10:47 -0600

    able to display image now

--- RMAapp/app/view.py
+++ RMAapp/app/view.py
@@ -8,22 +8,31 @@ import math
 @app.route('/filter')
 def filter():
 
-    zpid = 3197980
-    requestZillowAPI.centerRequest(zpid)
-
     from app.models import Apartment, Address
     from app.query import session
     apartments = session.query(Apartment).all()
 
-    localAp = []
-    centerZipcode = 61801
+    ## if you want to only display the apartments in a certain city, put the city
+    ## zipcode at centerZipcode, and enable the following lines
+    # localAp = []
+    # centerZipcode = 61801
+    #
+    # for apartment in apartments:
+    #     if apartment.address[0].zipcode == centerZipcode:
+    #         localAp.append(apartment)
+    # apartments = localAp
 
-    for apartment in apartments:
-        if apartment.address[0].zipcode == centerZipcode:
-            localAp.append(apartment)
-    apartments = localAp
+    def length(a):
+        return len(a)
 
     return render_template('index.html', title='Home', \
                             apartments=apartments, \
                             rows=math.ceil(len(apartments) / 3),\
-                            length=len(apartments))
+                            length=len(apartments), \
+                            len=length)
+
+@app.route('/update_database')
+def update_database():
+    zpid = 3197980
+    requestZillowAPI.centerRequest(zpid)
+    return 'Database updated'
