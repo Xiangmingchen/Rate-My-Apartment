755a9091c518df000454c83c6a1291e5f5637931
Rick <rikuiwanaga@users.noreply.github.com>
2017-12-07 17:03:18 -0600

    changed files

--- FlaskWTF/routes.py
+++ FlaskWTF/routes.py
@@ -1,22 +1,45 @@
-from flask import Flask, render_template, request
-
-
-#  Dont read: In order to generate the csrf token, you must have a secret key, this is usually the same as your
-#  Flask app secret key. If you want to use another secret key, config it:
-
+from flask import Flask, render_template, request, redirect
+from flask_wtf import Form
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired
 app = Flask(__name__)
-app.secret_key = 'CS196'
+app.secret_key = 'CS196'  # do we need this?
 
 
-@app.route('/form', methods=['GET', 'POST'])
-def review():
-    form = ContactForm()  # why is this not working?
+class ContactForm(Form):
+    name = StringField('username', validators=[DataRequired()])
+    subject = TextAreaField("subject", validators=[DataRequired()])
+    reviews = TextAreaField('reviews', validators=[DataRequired()])
+    submit = SubmitField("submit", validators=[DataRequired()])
 
-    if request.method == 'POST':
-        return "Review Posted."
-    elif request.method == 'GET':
-        return render_template('form.html', form=form)
+
+@app.route('/', methods=('GET', 'POST'))
+def submit():
+    form = ContactForm()
+    if form.validate_on_submit():  # meaning if form is submitted successfully.
+        return "Thank you for posting the review!"  # or redirect('/success')
+    return render_template('form.html', form=form)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(debug=True)
+
+
+# @app.route('/send', methods=['GET', 'POST'])
+# def send():
+#
+#     if request.method == 'POST':
+#         name = request.form['name']
+#         subject = request.form['subject']
+#         reviews = request.form['reviews']
+#
+#         return "Review Posted." and render_template('form.html', name=name, subject=subject, reviews=reviews)
+#
+#     elif request.method == 'GET':
+#         form = ContactForm()
+#         return render_template('form.html', form=form)
+#
+#
+# @app.route('/success')
+# def thanks():
+#     return "Your Review is Submitted!"
