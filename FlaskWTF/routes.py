2cf061f2a26372524e2bc33dbe2a049294133457
Rick <rikuiwanaga@users.noreply.github.com>
2017-12-07 17:44:23 -0600

    everything works

--- FlaskWTF/routes.py
+++ FlaskWTF/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template  # request, redirect
+from flask import Flask, render_template
 from flask_wtf import Form
 from wtforms import StringField, TextAreaField
 from wtforms.validators import InputRequired
