625508ba85bef73d9d7e7725dea0febb1a3c8aa6
Xiangmingchen <31719253+Xiangmingchen@users.noreply.github.com>
2017-12-09 16:37:23 -0600

    added city table to increase search speed, added amentities on reivew

--- RMAapp/app/models.py
+++ RMAapp/app/models.py
@@ -12,16 +12,29 @@ class Apartment(db.Model):
     address = relationship('Address', backref='apartment', cascade='save-update, merge, delete')
     rentPerMonth = Column(Float)
     image = relationship('Image', backref='apartment', cascade='save-update, merge, delete')
-    image_count = Column(Integer)
+    image_count = Column(Integer, default=0)
     review = relationship('Review', backref='apartment', cascade='save-update, merge, delete')
     comps = Column(Boolean, default=False)
     details = relationship('Details', backref='apartment', cascade='save-update, merge, delete')
     rooms = relationship('Rooms', backref='apartment', cascade='save-update, merge, delete')
     amentities = relationship('Amentities', backref='apartment', cascade='save-update, merge, delete')
     descripion = Column(String(2500))
+    review_number = Column(Integer, default=0)
+    average_rating = Column(Float, default=0)
+    city_id = Column(Integer, ForeignKey('city.id'))
 
     def __repr__(self):
         return '<Apartment %r>' % str(self.id)
+
+class City(db.Model):
+    __tablename__ = 'city'
+    # each city is related to the apartments in it
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    apartments = relationship('Apartment', backref='city', cascade='save-update, merge, delete')
+
+    def __repr__(self):
+        return '<City %r>' % str(self.name)
 
 class Details(db.Model):
     __tablename__ = 'details'
@@ -39,6 +52,7 @@ class Details(db.Model):
     parking_type = Column(String(20))
     heating_source = Column(String(20))
     heating_system = Column(String(20))
+    cooling_system = Column(String(20))
 
 class Rooms(db.Model):
     __tablename__ = 'rooms'
@@ -87,6 +101,8 @@ class Review(db.Model):
     id = Column(Integer, primary_key=True)
     user_name = Column(String(50), nullable=False)
     content = Column(Text, nullable=False)
+    rating = Column(Float, default=0)
+    timestamp = Column(String)
     apartment_id = Column(Integer, ForeignKey('apartment.id'))
 
     def __repr__(self):
