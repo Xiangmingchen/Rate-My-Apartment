c1ea6283c0c4523c7a84666e4b8632b5c1f9748c
Xiangmingchen <31719253+Xiangmingchen@users.noreply.github.com>
2017-12-10 14:02:47 -0600

    fixed form error message, final 1.2

--- RMAapp/app/forms.py
+++ RMAapp/app/forms.py
@@ -6,14 +6,6 @@ class SearchForm(FlaskForm):
     search = StringField('search', validators=[DataRequired()])
 
 class ReviewForm(FlaskForm):
-    username = StringField('Username', [validators.Length(min=4, max=25)])
+    username = StringField('Username', [validators.Length(min=2, max=25)])
     rating = RadioField('Rating', [validators.InputRequired()], choices=[("1", 'one'), ("2", 'two'), ("3", 'three'),("4",'four'),("5", 'five')])
     content = TextAreaField('Content', [validators.Length(min=10, max=100000)])
-
-
-class TestForm(FlaskForm):
-    choice_switcher = RadioField(
-        'Choice?',
-        [validators.Required()],
-        choices=[('choice1', 'Choice One'), ('choice2', 'Choice Two')], default='choice1'
-    )
